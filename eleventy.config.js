1b95b51d90f7fefccc7cc636ae44d2dcbc084b37
Christian Morrow <cmorrow415@outlook.com>
2025-11-29 21:58:23 -0500

    Begin expanding file structure

---
+++ eleventy.config.js
@@ -0,0 +1,8 @@
+module.exports = (config) => {
+	return {
+		dir: { //editing directory shit!
+			input: '_src', //look for files in _src...
+			output: '_public', //...and output them to _public!
+		},
+	};
+};
